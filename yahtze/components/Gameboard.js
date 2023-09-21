f647f0a36bd4588fa35077ec2f8d84c9e8f63735
Santti22 <n0koja00@students.oamk.fi>
2023-09-21 20:38:12 +0300

    added dice and made a loop for the grid items, more compact look on the rendered section of gameboard js file

--- yahtze/components/Gameboard.js
+++ yahtze/components/Gameboard.js
@@ -6,47 +6,71 @@ import styles from '../style/style';
 
 let board = [];
 
-// Array and loop for spot count
-let placeholderNums = [];
+
+// Loop to print cols and rows to make code more compact
+
+let gridArray = [];
 
 for (let i = 0; i < 6; i++) {
-    placeholderNums.push(
-        <MaterialCommunityIcons
-            name={'numeric-' + (i+1) + '-circle'}
+    gridArray.push(
+        <Col>
+            <Row name={'item' + i}
             key={"row" + i}
             size={35}
             color={'steelblue'}>
-        </MaterialCommunityIcons>
+                0
+            </Row>
+            <Row>
+                <MaterialCommunityIcons
+                    name={'numeric-' + (i+1) + '-circle'}
+                    key={"row" + i}
+                    size={35}
+                    color={'steelblue'}>
+                </MaterialCommunityIcons>
+            </Row>
+        </Col>
     )
 }
-
-// Count dice numbers and assign them here
-let pointArray = [];
-
-for (let i = 0; i < 6; i++) {
-    pointArray.push(
-        <View
-            name={'item'}
-            key={"row" + i}
-            size={35}
-            color={'steelblue'}>0
-        </View>
-    )
-}
-
 
 const NBR_OF_THROWS = 3;
+const NBR_OF_DICES = 5;
 const BONUSAMOUNT = 63;
-const TURNS = 0;
+const TURNS = 13;
+const newScore = 0;
 
 export default function Gameboard() {
 
     const [status, setStatus] = useState('Roll dice');
-    const [select, setSelect] = useState(false)
-    const [total, setTotal] = useState(0)
+    const [select, setSelect] = useState(false);
+    const [total, setTotal] = useState(0);
+
+    const [nbrOfThrowsLeft, setNbrOfThrowsLeft] = useState(NBR_OF_THROWS)
+
+    let row = [];
+
+    function throwDices() {
+
+        for (let i = 0; i < NBR_OF_DICES; i++) {
+            let randomNumber = Math.floor(Math.random() * 6 + 1);
+            board[i] = 'dice-' + randomNumber;
+        }
+        setNbrOfThrowsLeft(nbrOfThrowsLeft-1);
+    }
+
+    for (let i = 0; i < NBR_OF_DICES; i++) {
+        row.push(
+            <MaterialCommunityIcons
+            name={board[i]}
+            key={"row" + i}
+            size={50}
+            color={'steelblue'}>
+            </MaterialCommunityIcons>
+        );
+    }
 
     return (
         <View style={styles.gameboard}>
+            <Text style={styles.item}>{row}</Text>
             <Text style={styles.gameinfo}>Throws left: {NBR_OF_THROWS}</Text>
             <Text style={styles.gameinfo}>{status}</Text>
             <Pressable style={styles.button}
@@ -56,32 +80,14 @@ export default function Gameboard() {
                     </Text>
                 </Pressable>
                 <Text style={styles.gameinfo}>Total: {total}</Text>
-                <Text style={styles.gameinfo}>You are {BONUSAMOUNT} points away from bonus</Text>
-                <Grid>
-                    <Col>
-                        <Row>{pointArray[0]}</Row>
-                        <Row>{placeholderNums[0]}</Row>
-                    </Col>
-                    <Col>
-                        <Row>{pointArray[1]}</Row>
-                        <Row>{placeholderNums[1]}</Row>
-                    </Col>
-                    <Col>
-                        <Row>{pointArray[2]}</Row>
-                        <Row>{placeholderNums[2]}</Row>
-                    </Col>
-                    <Col>
-                        <Row>{pointArray[3]}</Row>
-                        <Row>{placeholderNums[3]}</Row>
-                    </Col>
-                    <Col>
-                        <Row>{pointArray[4]}</Row>
-                        <Row>{placeholderNums[4]}</Row>
-                    </Col>
-                    <Col>
-                        <Row>{pointArray[5]}</Row>
-                        <Row>{placeholderNums[5]}</Row>
-                    </Col>
+                <Text style={styles.gameinfo}>You are {BONUSAMOUNT - newScore} points away from bonus</Text>
+                <Grid style={styles.gameinfo}>
+                    {gridArray[0]}
+                    {gridArray[1]}
+                    {gridArray[2]}
+                    {gridArray[3]}
+                    {gridArray[4]}
+                    {gridArray[5]}
                 </Grid>
         </View>
     )
