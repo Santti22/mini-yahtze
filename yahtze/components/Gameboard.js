07623734be2f855814553b297dd6b5b65744d110
Santti22 <n0koja00@students.oamk.fi>
2023-09-21 19:19:22 +0300

    added react native grid library for rendering

--- yahtze/components/Gameboard.js
+++ yahtze/components/Gameboard.js
@@ -1,16 +1,39 @@
 import React, {useState, useEffect} from "react";
 import { Text, View, Pressable } from 'react-native';
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { Entypo } from '@expo/vector-icons';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 import styles from '../style/style';
 
 let board = [];
 
-// Array for numbers at bottom
-let placeholderNums = [1, 2, 3, 4, 5, 6];
+// Array and loop for spot count
+let placeholderNums = [];
+
+for (let i = 0; i < 6; i++) {
+    placeholderNums.push(
+        <MaterialCommunityIcons
+            name={'numeric-' + (i+1) + '-circle'}
+            key={"row" + i}
+            size={35}
+            color={'steelblue'}>
+        </MaterialCommunityIcons>
+    )
+}
 
 // Count dice numbers and assign them here
 let pointArray = [];
+
+for (let i = 0; i < 6; i++) {
+    pointArray.push(
+        <View
+            name={'item'}
+            key={"row" + i}
+            size={35}
+            color={'steelblue'}>0
+        </View>
+    )
+}
+
 
 const NBR_OF_THROWS = 3;
 const BONUSAMOUNT = 63;
@@ -34,8 +57,32 @@ export default function Gameboard() {
                 </Pressable>
                 <Text style={styles.gameinfo}>Total: {total}</Text>
                 <Text style={styles.gameinfo}>You are {BONUSAMOUNT} points away from bonus</Text>
-                <Text style={styles.gameinfo}></Text>
-                <Text style={styles.gameinfo}>{placeholderNums}</Text>
+                <Grid>
+                    <Col>
+                        <Row>{pointArray[0]}</Row>
+                        <Row>{placeholderNums[0]}</Row>
+                    </Col>
+                    <Col>
+                        <Row>{pointArray[1]}</Row>
+                        <Row>{placeholderNums[1]}</Row>
+                    </Col>
+                    <Col>
+                        <Row>{pointArray[2]}</Row>
+                        <Row>{placeholderNums[2]}</Row>
+                    </Col>
+                    <Col>
+                        <Row>{pointArray[3]}</Row>
+                        <Row>{placeholderNums[3]}</Row>
+                    </Col>
+                    <Col>
+                        <Row>{pointArray[4]}</Row>
+                        <Row>{placeholderNums[4]}</Row>
+                    </Col>
+                    <Col>
+                        <Row>{pointArray[5]}</Row>
+                        <Row>{placeholderNums[5]}</Row>
+                    </Col>
+                </Grid>
         </View>
     )
 }
